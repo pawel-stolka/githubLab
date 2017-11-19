e0bc216399abdf7b81aba896e3d9d6a2a4e9ef84
Pablo <pawel.stolka@gmail.com>
2017-11-19 23:07:42 +0100

    *ngFor - playlists-list

--- src/app/playlists/playlists-list.component.ts
+++ src/app/playlists/playlists-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist } from './playlist';
 
 @Component({
   selector: 'playlists-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playlists-list.component.css']
 })
 export class PlaylistsListComponent implements OnInit {
+
+  selected: Playlist;
+
+  playlists:Playlist[] = [
+    { id:1, name:"*ngIf",color: '#dd0000', fcolor: '#fff'},
+    { id:2, name:"*ngFor",color: '#cc0000', fcolor: '#aaaa00'},
+    { id:3, name:"Input()",color: '#000000', fcolor: '#fff'},
+    { id:4, name:"Output()",color: '#000000', fcolor: '#fff'}
+  ]
 
   constructor() { }
 
