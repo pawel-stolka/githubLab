fdfd0a1fee8cc90f5d4cda52c54e902a943bdada
Pablo <pawel.stolka@gmail.com>
2017-11-19 19:40:07 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { PlaylistsListComponent } from './playlists/playlists-list.component';
+import { PlaylistItemComponent } from './playlists/playlist-item.component';
+import { PlaylistDetailsComponent } from './playlists/playlist-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistsComponent,
+    PlaylistsListComponent,
+    PlaylistItemComponent,
+    PlaylistDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/playlists/playlists-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'playlists-list',
+  templateUrl: './playlists-list.component.html',
+  styleUrls: ['./playlists-list.component.css']
+})
+export class PlaylistsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
