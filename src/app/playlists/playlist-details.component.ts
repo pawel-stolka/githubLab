f759332c31e1e63a724eff870b01af8acc665721
Pablo <pawel.stolka@gmail.com>
2017-11-19 20:18:57 +0100

    2

--- src/app/playlists/playlist-details.component.ts
+++ src/app/playlists/playlist-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Playlist } from './playlist';
 
 @Component({
   selector: 'playlist-details',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./playlist-details.component.css']
 })
 export class PlaylistDetailsComponent implements OnInit {
+
+  playlist: Playlist = {
+    id: 1,
+    name: "Learning after Mateusz Workshops",
+    fcolor: '#fff',
+    color: '#123456'
+  }
 
   constructor() { }
 
