4f391e7ab06edcab0a96a44e0ed31559bc16eae7
CUBEITG\pstolka <pstolka@cubeitg.pl>
2017-11-21 12:48:12 +0100

    authentication

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -11,8 +11,12 @@ import 'rxjs/add/operator/map'
 export class GithubComponent implements OnInit {
   userInfo: any;
   userBase = 'https://api.github.com/users/';
+  query: string = 'octocat'; 
   username: string = 'octocat';
   // results: any;
+  client_id='d26dbb03b3670269a61e';
+  client_secret='0c4fb6aeba85678269c84ba55ce0ccf9c842ac75';
+  userTokenParam = '?client_id='+this.client_id+'&client_secret='+this.client_secret;
   fetched: boolean = false;
   followersUrl: string;
   followers: number;
@@ -23,37 +27,41 @@ export class GithubComponent implements OnInit {
 
   // constructor(private service: GithubService) { }
 
-  getUser() {
-    let url = this.userBase + this.username
-    this.followersUrl = url + '/followers'
+  getUser(query) {
+    let url = this.userBase + query;// this.username
+    this.followersUrl = url + '/followers' + this.userTokenParam;
+    console.log('followersUrl: ' + this.followersUrl);
     this.http.get(url)
-    .subscribe(
-      data => {
-        this.userInfo = data;
-        console.log(this.userInfo)
-        this.fetched = true;
-        this.getFollowers()
-        },
-      err => console.log('Something went wrong...')
-    )
+      .subscribe(
+        data => {
+          this.userInfo = data;
+          console.log(this.userInfo)
+          this.fetched = true;
+          this.getFollowers()
+          },
+        err => {
+          console.log('Something went wrong...: ')
+          console.log(err.error.message)
+        }
+      )
   }
 
   getFollowers() {
     this.followersAll = [];
     this.http.get(this.followersUrl)
     // .map( () => { console.log("data")})
-    .subscribe(
-      data => {
-        // console.log(data)
-        this.followers = Object.keys(data).length
-        for(let key in data) {
-          console.log(data[key])
-          this.followersAll.push(data[key])
-        }
-        
-      },
-      err => console.log('followers went wrong...')
-    )
+      .subscribe(
+        data => {
+          // console.log(data)
+          this.followers = Object.keys(data).length
+          for(let key in data) {
+            console.log(data[key])
+            this.followersAll.push(data[key])
+          }
+          
+        },
+        err => console.log('followers went wrong...')
+      )
     
   }
 
